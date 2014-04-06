a34ed5f0fced2b676c72b46826cbf7b4d6b1c9aa
Alex Irpan <xelanapri@gmail.com>
2014-04-05 23:58:55 -0700

    Can find card locations on dark background

--- cardReader.py
+++ cardReader.py
@@ -9,11 +9,40 @@ extension = "*.jpg"
 directory = os.path.join(cards_path, extension)
 files = glob.glob(directory)
 
+size_threshold = 6000 # max pixels in box
+ratio = 1.35 # taken from a picture
+ratio_threshold = 0.2
+
 for file in files:
-    img = Image(file).scale(0.1)
+    print "New image"
+    img = Image(file).scale(0.2)
     img.show()
     time.sleep(0.5)
-    img.edges().show()
+    modified = img
+    modified.show()
     time.sleep(0.5)
-    img.binarize().show()
+    edges = modified.edges()
+    blobs = edges.findBlobs()
+    edges.show()
     time.sleep(0.5)
+    boxlayer = DrawingLayer((img.width, img.height))
+    to_crop_to = []
+    for blob in blobs:
+        x, y, w, h = blob.boundingBox()
+        imgratio = max(w,h) / float(min(w,h))
+        print w*h
+        if w*h >= size_threshold:
+            boxlayer.rectangle((x, y), (w,h), width=2, color=Color.GREEN)
+            to_crop_to.append((x,y,w,h))
+            print max(w,h) / float(min(w,h))
+    img.addDrawingLayer(boxlayer)
+    img.applyLayers()
+    img.show()
+    time.sleep(0.5)
+    
+    for c in to_crop_to:
+        img.crop(*c).show()
+        time.sleep(0.5)
+    
+        
+    
