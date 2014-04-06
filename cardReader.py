f548c069b159709c75fcd211757032f62d3fb2a4
Alex Irpan <xelanapri@gmail.com>
2014-04-06 11:24:15 -0700

    Final push to hopefully get this to work

--- cardReader.py
+++ cardReader.py
@@ -16,11 +16,11 @@ ratio_threshold = 0.2
 def output_cards(img):
     modified = img
     edges = modified.edges()
+    edges.show()
+    time.sleep(1)
     blobs = edges.findBlobs()
     #boxlayer = DrawingLayer((img.width, img.height))
     to_crop_to = []
-    edges.findBlobs().show()
-    time.sleep(1)
     for blob in blobs:
         x, y, w, h = blob.boundingBox()
         imgratio = max(w,h) / float(min(w,h))
@@ -34,7 +34,6 @@ def output_cards(img):
     # face cards have blobs in the center, which we don't want
     # check if a box contains another, remove inner box
     boxes = list(to_crop_to)
-    print boxes
     for x,y,w,h in to_crop_to:
         remove = False
         for x2,y2,w2,h2 in to_crop_to:
@@ -49,55 +48,40 @@ def output_cards(img):
     cropped = [img.crop(*c) for c in boxes]
     return cropped
 
+def find_color(card_img):
+    # Takes normalized card
+    # Get the top left corner (roughly)
+    rank_and_suit = card_img.crop(8,8,24,64)
+    rank_and_suit.show()
+    time.sleep(1)
+    # count red pixels and black pixels
+    # red: RGB value has R as largest value, R is above 100
+    # black: Each value below threshold
+    black_threshold = 50
+    red_pix = 0
+    black_pix = 0
+    
+    flat = rank_and_suit.getNumpy()
+    for i in range(len(flat)):
+        for j in range(len(flat[0])):
+            b, g, r = flat[i, j]
+            if r > 100 and r == max(b,g,r):
+                red_pix += 1
+            elif max(r,g,b) < black_threshold:
+                black_pix += 1
+    print "Red ", red_pix, "Black ", black_pix
+    if black_pix > 100:
+        print "Guessing a black card"
+    else:
+        print "Guessing a red card"
+    
 suits_path = "suits/"
 extension = "*.png"
 suit_imgs = glob.glob(os.path.join(suits_path, extension))
     
 def check_suit(card_img):
-    t = 5
-
-    methods = ["SQR_DIFF","SQR_DIFF_NORM","CCOEFF","CCOEFF_NORM","CCORR","CCORR_NORM"] # the various types of template matching available
-    for m in methods:
-        copy = card_img.scale(1)
-        print "current method:", m # print the method being used
-        dl = DrawingLayer((copy.width, copy.height))
-        colors = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW]
-        curr = 0
-        for suit in suit_imgs:
-            fs = copy.findTemplate(Image(suit), threshold=t, method=m)
-            for match in fs:
-                dl.rectangle((match.x,match.y),(match.width(),match.height()),color=colors[curr])
-            curr += 1
-        copy.addDrawingLayer(dl)
-        copy.applyLayers()
-        copy.show()
-        time.sleep(1.5)
-    
-def read_card(card_img):
-    # normalize card
-    if card_img.width > card_img.height:
-        card_img = card_img.rotate(90, fixed=False)
-    normalized_size_width = 186.0# random value
-    card_img = card_img.scale(card_img.width / normalized_side_width)
-    # at normalized size,
-    suit_width = 18
-    suit_height = 22 # +/- a bit
-    
-    card_img.show()
-    time.sleep(1.5)
-    card_img.edges().show()
-    time.sleep(1.5)
-    check_suit(card_img)
-    blobs = card_img.edges().findBlobs()
-    count = 0
-    boxlayer = DrawingLayer((card_img.width, card_img.height))
-    boxes = [blob.boundingBox() for blob in blobs]
-    for box in boxes:
-        x,y,w,h = box
-        boxlayer.rectangle((x, y), (w,h), width=2, color=Color.GREEN)
-        count += 1
-    # find the top left and bottom right boxes
-    # this only does leftmost + rightmost but works on cards
+    find_color(card_img)
+    return
     boxes.sort(key=lambda b: b[0])
     boxlayer.rectangle(boxes[0][:2], boxes[0][2:], width=2, color=Color.RED)
     boxlayer.rectangle(boxes[1][:2], boxes[1][2:], width=2, color=Color.RED)
@@ -105,16 +89,57 @@ def read_card(card_img):
     boxlayer.rectangle(boxes[0][:2], boxes[0][2:], width=2, color=Color.BLUE)
     boxlayer.rectangle(boxes[1][:2], boxes[1][2:], width=2, color=Color.BLUE)
     
-    card_img.addDrawingLayer(boxlayer)
-    card_img.applyLayers()
-    card_img.show()
-    print "guess of %d" % count
-    time.sleep(1.5)
+    methods = ["SQR_DIFF","SQR_DIFF_NORM","CCOEFF","CCOEFF_NORM","CCORR","CCORR_NORM"] # the various types of template matching available
+    for m in methods:
+        copy = card_img.scale(1)
+        print "current method:", m # print the method being used
+        dl = DrawingLayer((copy.width, copy.height))
+        colors = [Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW]
+        curr = 0
+        for name, suit in zip(('club', 'diamond', 'heart', 'spade'), suit_imgs):
+            fs = copy.findTemplate(Image(suit), method=m)
+            if fs:
+                print "%s found" % name
+            for match in fs:
+                dl.rectangle((match.x,match.y),(match.width(),match.height()),color=colors[curr])
+            curr += 1
+        copy.addDrawingLayer(dl)
+        copy.applyLayers()
+        copy.show()
+        time.sleep(0.75)
     
+def read_card(card_img):
+    # normalize card
+    if card_img.width > card_img.height:
+        card_img = card_img.rotate(90, fixed=False)
+    normalized_side_width = 200.0 # random value
+    card_img = card_img.scale(card_img.width / normalized_side_width)
+    # at normalized size,
+    suit_width = 18
+    suit_height = 22 # +/- a bit
+    
+    card_img.show()
+    time.sleep(0.75)
+    # check_suit(card_img)
+    blobs = card_img.edges().findBlobs()
+    count = 0
+    boxlayer = DrawingLayer((card_img.width, card_img.height))
+    boxes = [blob.boundingBox() for blob in blobs]
+    rank = find_rank(card_img, boxes)
+    suit = find_suit(card_img, boxes)
+    # display this guess
+
+def find_rank(card_img, boxes):
+    # Debug drawing
+    for box in boxes:
+        x,y,w,h = box
+        boxlayer.rectangle((x, y), (w,h), width=2, color=Color.GREEN)
+        
+        
 import random
 for file in files:
     print "New image"
-    img = Image(file).scale(0.2)
+    img = Image(file, colorSpace=ColorSpace.BGR).scale(0.2)
     cards = output_cards(img)
     for card in cards:
         read_card(card)
